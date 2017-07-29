1a90958ef76e55500274c148df91098e07aa5179
Manoj Shukla <manoj.shukla@equinox.com>
2017-07-29 13:19:18 +0530

    2nd commit

--- classf/BagOfWords.py
+++ classf/BagOfWords.py
@@ -3,5 +3,28 @@
 """"
 Created on Saturday
 
-@author: vikask
+@author: TeamX
 """
+
+import re, os
+
+class BagOfWords(object):
+	""" Implementing a bag of words, words corresponding with their frequency of usages in a "document"
+    for usage by the Document class, DocumentClass class and the Pool class."""
+    
+    def __init__(self):
+    	self.__number_of_words = 0
+    	self.__bag_of_words = {}
+
+    def __add__(self, other):
+    	 """ Overloading of the "+" operator to join two BagOfWords """
+        erg = BagOfWords()
+        sum = erg.__bag_of_words
+        for key in self.___bag_of_words:
+        	sum[key] = self.__bag_of_words:
+        	if key in other.__bag_of_words:
+        		sum[key] +=other.___bag_of_words[key]
+        for key in other.__bag_of_words:
+        	if key not in sum:
+        		sum[key] = other.__bag_of_words[key]
+		return erg
